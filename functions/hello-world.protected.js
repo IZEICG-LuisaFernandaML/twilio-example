58c48b4b04a227d4c98f7acc1f43114f63102de0
IZEICG-LuisaFernandaML <lmorales@izeicg.com>
2026-02-11 19:54:23 -0700

    quitando comentarios

--- functions/hello-world.protected.js
+++ functions/hello-world.protected.js
@@ -3,6 +3,3 @@ exports.handler = function(context, event, callback) {
   twiml.say('Hello World!');
   callback(null, twiml);
 };
-
-//pfuncion con parametros sencillos (p.e., nombre), y que la funcion diga algo asi como "hola ${nombre} como estas"
-//que sea sencillo para aprender como se llama a la funcion. Probarla con postman; peticiones con puro POST
