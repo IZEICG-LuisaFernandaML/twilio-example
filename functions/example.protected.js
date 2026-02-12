4537ffcf2ff5c4e5fadb884077952a0fd3a1f32c
IZEICG-LuisaFernandaML <lmorales@izeicg.com>
2026-02-11 23:03:07 -0700

    modificando el mensaje de bienvenida; se agrego el parametro opcional de edad

--- functions/example.protected.js
+++ functions/example.protected.js
@@ -1,6 +1,15 @@
 exports.handler= function (context, event,callback){
-    const receivedName = event.name;
-    const twiml = new Twilio.twiml.MessagingResponse();
-    twiml.message(`Hello ${receivedName}!!`);
-    callback(null,twiml);
+    const {name, age} = event;
+    const response = new Twilio.Response();
+    response.appendHeader('Content-Type','application/json');
+    if(!name){
+        response.setStatusCode(400).setBody({error:'The name is required'});
+    }else{
+        const baseMsg= context.MENSAJE_BASE || ', te recuerdo que ya casi tienes';
+        const msg = `Hola, ${name}${(age? `${baseMsg} ${Number(age)+1} años!` :``)}`;
+        
+        response.setStatusCode(200).setBody({message:msg});
+    }
+
+    callback(null,response);
 };
